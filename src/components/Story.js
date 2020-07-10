ddab0083212ba26b89fbf9f985bd117dd2981912
Rajan Gurung <rajangrg1@hotmail.com>
2020-07-10 16:09:07 +1200

    time format fixed

--- src/components/Story.js
+++ src/components/Story.js
@@ -8,6 +8,7 @@ import {
   StoryMeta,
   StoryMetaElement,
 } from "../styles/StoryStyles";
+import { mapTime } from "../mappers/mapTime";
 
 export const Story = ({ storyId }) => {
   const [story, setStory] = useState({});
@@ -37,7 +38,7 @@ export const Story = ({ storyId }) => {
             </span>
             <span data-testid="story-time">
               <StoryMetaElement color="#DE5D3A">Posted: </StoryMetaElement>
-              {story.time}
+              {mapTime(story.time)}
             </span>
           </StoryMeta>
         </StoryWrapper>
